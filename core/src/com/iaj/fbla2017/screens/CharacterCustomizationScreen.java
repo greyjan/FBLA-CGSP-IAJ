47da5ebca99985c3882782beffb911728860828b
Jan Fic <janfc6@gmail.com>
2017-10-25 20:27:50 -0400

    added assets stuff

---
+++ core/src/com/iaj/fbla2017/screens/CharacterCustomizationScreen.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iaj.fbla2017.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ *
+ * @author Jan Fic
+ */
+public class CharacterCustomizationScreen implements Screen{
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+    
+}
