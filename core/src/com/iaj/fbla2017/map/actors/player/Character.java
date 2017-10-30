26bc3de84fee7cd4dc584489e3c764bb7d77ba5d
n080873 <janfc6@gmail.com>
2017-10-30 09:18:22 -0400

    changed player to character class

---
+++ core/src/com/iaj/fbla2017/map/actors/player/Character.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iaj.fbla2017.map.actors.player;
+
+/**
+ *
+ * @author Jan Fic
+ */
+public class Character{
+    /** Fields
+     * Jawline
+     * Hair
+     * Clothes
+     * Inventory
+     * Body
+     */
+} 
