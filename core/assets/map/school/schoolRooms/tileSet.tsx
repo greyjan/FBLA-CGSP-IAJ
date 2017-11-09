8dedaf82e6dbe92d1a11ff218ae068ac74c49498
n342432 <amwood1026@gmail.com>
2017-11-09 09:58:01 -0500

    smaller but bigger at the same time

---
+++ core/assets/map/school/schoolRooms/tileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="isometricTiles" tilewidth="32" tileheight="32" tilecount="52" columns="13">
+ <image source="../../tileSet/tiles.png" width="416" height="128"/>
+</tileset>
