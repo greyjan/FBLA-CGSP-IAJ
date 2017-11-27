01b1d0d6ef4446b03988d084d154c2d4ba723968
n342432 <amwood1026@gmail.com>
2017-11-27 09:58:45 -0500

    asdf

---
+++ core/src/com/iaj/fbla2017/map/objects/Door.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iaj.fbla2017.map.objects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.iaj.fbla2017.assets.Assets;
+
+/**
+ *
+ * @author n342432
+ */
+public class Door extends Furniture{
+    
+    public Door(int x, int y, String direction) {
+        super(x, y);
+        sprite = new Sprite((Texture) Assets.GetInstance().get(Furniture.FURNATURE_PATH + "door" + direction + ".png"));
+        this.setBounds(x, y, sprite.getWidth(), sprite.getHeight());
+        this.setIsoX((x - 16 ) / (16));
+        this.setIsoY((y) / (16));
+    }
+    
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.setColor(this.getColor());
+        batch.draw(sprite, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+    }
+    
+}
